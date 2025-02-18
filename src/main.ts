0f5c87b95198274a1740a027ef6413a4512c80a2
Sassyprogrammer1 <hamzajamshed.cs@gmail.com>
2025-02-19 03:14:05 +0400

    feat: add sls nest integration

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,33 @@
 import { NestFactory } from '@nestjs/core';
+import serverlessExpress from '@codegenie/serverless-express';
+import { Callback, Context, Handler } from 'aws-lambda';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
 
-async function bootstrap() {
+dotenv.config(); // Load environment variables from .env file
+
+let server: Handler;
+
+// Function to bootstrap NestJS for AWS Lambda
+async function bootstrapServerless(): Promise<Handler> {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.enableCors({
+    origin: '*', // Allow all origins
+  });
+
+  await app.init();
+
+  const expressApp = app.getHttpAdapter().getInstance();
+  return serverlessExpress({ app: expressApp });
 }
-bootstrap();
+
+// Lambda handler function for AWS Lambda (serverless)
+export const authHandler: Handler = async (
+  event: any,
+  context: Context,
+  callback: Callback,
+) => {
+  server = server ?? (await bootstrapServerless());
+  return server(event, context, callback);
+};
